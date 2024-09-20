6ea619754387bb074dcedadbebb51dfda1d4cb30
CareyK25 <careyk25@stjohnshigh.org>
2024-09-20 14:44:10 -0400

    aa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOmniOpMode_Linear.java
@@ -67,6 +67,25 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class BasicOmniOpMode_Linear extends LinearOpMode {
 
+    // waiugfwiugiwaigshgipushg
+
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
+    // waiugfwiugiwaigshgipushg
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
