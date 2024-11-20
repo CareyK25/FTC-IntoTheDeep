a1fb0e45e2cfeb23f8d65c47a99c26dfe0f27769
CareyK25 <careyk25@stjohnshigh.org>
2024-11-20 14:41:46 -0500

    added hardware mapper class and began work on changing input code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/InputDevice.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.control;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class InputDevice {
+
+
+
+    volatile Gamepad device;
+
+
+    public InputDevice(Gamepad device) {
+        this.device = device;
+    }
+
+
+
+
+
+
+
+}
