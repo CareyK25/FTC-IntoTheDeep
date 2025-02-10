98b4089e2346232f8a4c375f26bb40340e78f5a1
jeremy <somarj@stjohnshigh.org>
2025-02-10 16:49:49 -0500

    Color Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.Arrays;
+
+@Autonomous(name = "ColorTest")
+public class ColorTest extends LinearOpMode {
+    public static ColorSensor color;
+
+    public void runOpMode() {
+        color = hardwareMap.get(ColorSensor.class, "color");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            int [] rgb = scan();
+            telemetry.addData("RGB vals", Arrays.toString(rgb));
+        }
+    }
+
+    public int[] scan() {
+        return new int[]{color.red(), color.green(), color.blue()};
+    }
+
+}
